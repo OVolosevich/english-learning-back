b9755b753ff2dbd76a21d64463da8e2caf8a10f5
Volha Valasevich <volha.valasevich@n1affiliates.com>
2023-10-18 21:47:50 +0200

    feat: add create users handler

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Header } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
   @Post()
+  @Header('Access-Control-Allow-Origin', 'http://localhost:3030')
+  @Header('Access-Control-Allow-Methods', 'GET, POST, OPIONS')
   create(@Body() createUserDto: CreateUserDto): string {
     return `This action adds a user with id ${createUserDto.id}`;
   }
